f84b1ed00b7031e061c87eef5786c91991de948d
saeleuterio <saeleuterio@hotmail.com>
2025-02-26 10:28:45 -0300

    up

--- scripts.js
+++ scripts.js
@@ -114,3 +114,17 @@ botaoPublicar.addEventListener("click", async (evento) => {
     }
     
 })
+
+const botaoDescartar = document.querySelector(".botao-descartar");
+
+botaoDescartar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+
+    const formulario = document.querySelector("form");
+    formulario.reset();
+
+    imagemPrincipal.src = "./img/imagem1.png";
+    nomeDaImagem.textContent = "imagem_projeto.png";
+
+    listaTags.innerHTML = "";
+})
